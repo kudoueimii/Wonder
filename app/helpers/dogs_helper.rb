bea566016eeaed6ddb464e878a51bf8db460cf14
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-21 10:22:29 +0900

    リファクタリング箇所洗い出し

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,9 +1,22 @@
 module DogsHelper
   def matching_result(dog)
     @user_choices = current_user.user_choice
-    user_choices = [@user_choices.vehicle, @user_choices.cleaning, @user_choices.active, @user_choices.exercise,
-                    @user_choices.home, @user_choices.house]
-    dog_choices = [dog.vehicle, dog.cleaning, dog.active, dog.exercise, dog.home, dog.house]
+    user_choices = [
+      @user_choices.vehicle, 
+      @user_choices.cleaning, 
+      @user_choices.active,
+      @user_choices.exercise,
+      @user_choices.home,
+      @user_choices.house
+    ]
+    dog_choices = [
+      dog.vehicle,
+      dog.cleaning,
+      dog.active,
+      dog.exercise,
+      dog.home,
+      dog.house
+    ]
     matching_count = 0
     user_choices.each_with_index do |user_choice, i|
       matching_count += 1 if user_choice == dog_choices[i]
